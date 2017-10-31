41914ce5cfc11157ed864cdf09db1bf048ee6e0f
‘Heath <heathsmith73@gmail.com>
2017-10-30 18:24:57 -0700

    working on it

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # bus-mall
 bus mall repo
+
+Hello!
+
+Welcome to the bus mall app.  This a is basic app for finding out what the customer whats.  The app with display 3 images for users to select. once the users clicks on the desired image. it will then clear the images and display 3 new images to pick from. this will happen 25 times.  once it is complete. the app will be finished.  Under the hood, the images will be stored with total amount of click on each image for later data to be displayed. 
---
+++ app.js
@@ -0,0 +1,91 @@
+'use strict';
+
+var panelOne = document.getElementById('panel-1');
+var panelTwo = document.getElementById('panel-2');
+var panelThree = document.getElementById('panel-3');
+
+var previousfirst;
+var previousSecond;
+var previousthird;
+var firstImageNumber;
+var secondImageNumber;
+var thirdImageNumber;
+var displayedImage = [];
+var imageArray = [
+  'bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen','pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'
+];
+
+
+function CreateImage(name) {
+  this.location = 'img/' + name + '.jpg';
+  this.name = name;
+  this.imageShown = 0;
+  this.imageClicked = 0;
+}
+
+//this take the imageArray and runs in in CreateImage to create objects that are then pushed to the new array called displayedImage.
+for (var i = 0; i < imageArray.length; i++) {
+  displayedImage.push(new CreateImage(imageArray[i]));
+}
+
+//this create a random num that is different then the other 2 numbers it creates.
+function randomNumGen() {
+  var min = 0;
+  var max = imageArray.length - 1;
+  var randomNum = Math.floor(Math.random() * (max - min) + 1);
+  while (randomNum === previousfirst || randomNum === previousSecond || randomNum === previousthird) {
+    randomNum = Math.floor(Math.random() * (max - min) + 1);
+  }
+  console.log('random number:', randomNum);
+  return randomNum;
+}
+
+
+function compareNumbers() {
+  firstImageNumber = randomNumGen();
+  secondImageNumber = randomNumGen();
+  thirdImageNumber = randomNumGen();
+  while (secondImageNumber === firstImageNumber) {
+    secondImageNumber = randomNumGen();
+  }
+  while (thirdImageNumber === firstImageNumber || thirdImageNumber === secondImageNumber) {
+    thirdImageNumber = randomNumGen();
+  }
+  previousfirst = firstImageNumber;
+  previousSecond = secondImageNumber;
+  previousthird = thirdImageNumber;
+
+  console.log('first number', previousfirst);
+  console.log('second number', previousSecond);
+  console.log('third number', previousthird);
+}
+
+//compareNumbers();
+
+function imagesToPage() {
+
+  for (var k = 0; k < 25; k++) {
+    compareNumbers();
+    var imageToShow = [];
+    imageToShow.push(displayedImage[firstImageNumber]);
+    imageToShow.push(displayedImage[secondImageNumber]);
+    imageToShow.push(displayedImage[thirdImageNumber]);
+
+    displayedImage[firstImageNumber].imageShown++;
+    displayedImage[secondImageNumber].imageShown++;
+    displayedImage[thirdImageNumber].imageShown++;
+
+    // for (var j = 0; j < imageToShow.length; j++) {
+    panelOne.innerHTML = '<img src="' + imageToShow[0].location + '">';
+    panelTwo.innerHTML = '<img src="' + imageToShow[1].location + '">';
+    panelThree.innerHTML = '<img src="' + imageToShow[2].location + '">';
+
+  }
+}
+
+imagesToPage();
+
+
+// var webListOne = document.getElementById('img-one');
+// var webListTwo = document.getElementById('img-two');
+// var webListThree = document.getElementById('img-three');
