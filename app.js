7e6aa2c149a482918d67e94c74fae272c4fa2126
‘Heath <heathsmith73@gmail.com>
2017-10-30 20:03:30 -0700

    added id

--- app.js
+++ app.js
@@ -15,18 +15,22 @@ var displayedImage = [];
 var imageArray = [
   'bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen','pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'
 ];
+var imageId = [
+  'id-1', 'id-2', 'id-3', 'id-4', 'id-5', 'id-5', 'id-6', 'id-7', 'id-8', 'id-9', 'id-10', 'id-11', 'id-12', 'id-13', 'id-14', 'id-15', 'id-16', 'id-17', 'id-18', 'id-19', 'id-20'
+];
 
 
-function CreateImage(name) {
+function CreateImage(name, id) {
   this.location = 'img/' + name + '.jpg';
   this.name = name;
   this.imageShown = 0;
   this.imageClicked = 0;
+  this.imageId = id;
 }
 
 //this take the imageArray and runs in in CreateImage to create objects that are then pushed to the new array called displayedImage.
 for (var i = 0; i < imageArray.length; i++) {
-  displayedImage.push(new CreateImage(imageArray[i]));
+  displayedImage.push(new CreateImage(imageArray[i], imageId[i]));
 }
 
 //this create a random num that is different then the other 2 numbers it creates.
@@ -75,14 +79,16 @@ function imagesToPage() {
   displayedImage[secondImageNumber].imageShown++;
   displayedImage[thirdImageNumber].imageShown++;
 
+  document.getElementById('random-images').innerHTML = ''; //resets the images.
+
   for (var j = 0; j < imageToShow.length; j++) {
     var imageLi = document.createElement('li');
     imageLi.innerHTML = '<img src="' + imageToShow[j].location + '">';
     imageUl.appendChild(imageLi);
     console.log(imageLi);
     console.log(imageToShow[j]);
+
   }
-  // console.log(imageUl.getElementsByTagName('li'));
 }
 
 imagesToPage();
